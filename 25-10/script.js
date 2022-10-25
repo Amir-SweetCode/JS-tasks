997fb5989185e17595c1e7312ae948e95d1f32ec
Amir <amir.2003.kg@gmail.com>
2022-10-25 11:05:40 +0600

    feat: added file 25-10

---
+++ 25-10/script.js
@@ -0,0 +1,17 @@
+
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7;
+let result = c + d;
+console.log(result);
+
+let hour = 10;
+let minute = 43;
+let second = 55;
+console.log(`${hour}:${minute}:${second}`)
+
+var num = 10;
+num1 = ++num;
+num2 = num++;
+alert(num2);

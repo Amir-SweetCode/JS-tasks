e0245bb9babe3abc4f8d004e93f39597c124b52a
Amir <amir.2003.kg@gmail.com>
2022-11-02 12:45:38 +0600

    feat: added 1-3 zadanie

--- 02-11/script.js
+++ 02-11/script.js
@@ -10,3 +10,26 @@ function number(result) {
     alert(result);
 }
 number(result);
+
+
+/********** 1 zadanie ***********/
+function hello() {
+    return 'Привет, Javascript';
+}
+console.log(hello());
+
+
+/********** 2 zadanie ***********/
+function number1(kub) {
+    return kub ** 3;
+}
+console.log(number1(3))
+
+
+/********** 3 zadanie ***********/
+let name = 'Василий';
+
+function getName(name = 'гость '){
+    return 'Привет, ' + name;
+}
+console.log(getName(name));
